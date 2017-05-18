5046c949908b3c268acab2b7d6ce994cc6a536ad
Mikunj varsani <mikunj@live.com.au>
2017-05-18 21:24:28 +1000

    Comments .. whoops

--- src/tweetanalyser.py
+++ src/tweetanalyser.py
@@ -1,5 +1,6 @@
 from datetime import date, datetime
 
+#Analyse an array of tweets and construct a frequency dictionary from them
 class TweetAnalyser():
     def __init__(self, tweets, converter):
         self.tweets = tweets
